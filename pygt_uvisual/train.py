5cf3e02f45cd4a58366dddf9c54d12cf48f3525e
Thanuja Ambegoda <thanujadax@gmail.com>
2016-08-09 14:37:43 +0200

    done tiff inputs

--- pygt_uvisual/train.py
+++ pygt_uvisual/train.py
@@ -11,7 +11,7 @@ pygt_path = '../../PyGreentea'
 
 
 import sys, os
-sys.path.append(os.path.join(os.path.dirname(__file__), pygt_path))
+sys.path.append(os.path.join(os.path.dirname('__file__'), pygt_path))
 
 # Other python modules
 import math
@@ -19,6 +19,9 @@ import math
 # Load PyGreentea
 import PyGreentea as pygt
 
+
+npv = np.version.version
+print("numpy version: {0}".format(npv))
 '''
 Input option 1
 Load the datasets - hdf5
@@ -49,12 +52,35 @@ Load the datasets - individual tiff files in a directory
 rawInputDir = '/home/thanuja/projects/data/dataset_01/train/raw';
 labelInputDir = '/home/thanuja/projects/data/dataset_01/train/labels'
 
-rawImagePath = glob.glob(rawInputDir+'/*.tif')
-labelImagePath = glob.glob(labelInputDir+'/*.png')
+rawImagePath = sorted(glob.glob(rawInputDir+'/*.tif'))
+print("rawImagePath: {0}".format(rawImagePath))
+labelImagePath = sorted(glob.glob(labelInputDir+'/*.png'))
 numFiles = len(rawImagePath)
 
 hdf5_raw_ds = np.array( [np.array(Image.open(rawImagePath[i]).convert('L'), 'f') for i in range(0,numFiles)] )
 hdf5_gt_ds = np.array( [np.array(Image.open(labelImagePath[i]).convert('L'), 'f') for i in range(0,numFiles)] )
+
+
+'''
+print("os.listdir(rawInputDir):{0}".format(os.listdir(rawInputDir)))
+raw_arrays =  []
+print("Start william's log message...")
+print("numFiles: {0}".format(numFiles))
+for i in range(numFiles):
+    path = rawImagePath[i]
+    image_object = Image.open(path)
+    image_object.convert('L')
+    image_array = np.array(image_object, 'f')
+    print(i, image_array.shape, image_array.dtype, image_object.size, image_object.info)
+    raw_arrays.append(image_array)
+hdf5_raw_ds = np.array(raw_arrays)
+print(hdf5_raw_ds.shape, hdf5_raw_ds.dtype)
+print("Stop william's log message.")
+'''
+
+print("hdf5_raw_ds.shape = {0}".format(hdf5_raw_ds.shape))
+print("hdf5_gt_ds.shape = {0}".format(hdf5_gt_ds.shape))
+
 ###############################################################
 '''
 Input option 3
@@ -73,9 +99,11 @@ tiff_aff = Image.open(tiff_aff_file)
 # tiff
 '''
 ###############################################################
-
+'''
 datasets = []
-for i in range(0,hdf5_raw_ds.shape[1]):
+# for i in range(hdf5_raw_ds.shape[1]):
+for i in range(hdf5_raw_ds.shape[0]):
+    # print(i)
     dataset = {}
     dataset['data'] = hdf5_raw_ds[None, i, :]
     dataset['components'] = hdf5_gt_ds[None, i, :]
@@ -83,11 +111,21 @@ for i in range(0,hdf5_raw_ds.shape[1]):
     # dataset['label'] = hdf5_aff_ds[0:3, i, :]
     dataset['nhood'] = pygt.malis.mknhood2d()
     datasets += [dataset]
+'''
+
+datasets = []
+for i in range(0,hdf5_raw_ds.shape[1]):
+    dataset = {}
+    dataset['nhood'] = pygt.malis.mknhood2d()
+    dataset['data'] = hdf5_raw_ds[None, i, :]
+    components_array = hdf5_gt_ds[i, :]
+    dataset['label'] = pygt.malis.seg_to_affgraph(components_array, dataset['nhood'])
+    dataset['components'] = components_array[None, :]
+    datasets += [dataset]
 
 #test_dataset = {}
 #test_dataset['data'] = hdf5_raw_ds
 #test_dataset['label'] = hdf5_aff_ds
-
 
 # Set train options
 class TrainOptions:
@@ -101,7 +139,6 @@ class TrainOptions:
     train_device = 0
     test_device = 0
     test_net=None #'net_test.prototxt'
-
 
 options = TrainOptions()
 
@@ -124,7 +161,6 @@ solver_config.display = 1
 # pygt.caffe.enumerate_devices(False)
 pygt.caffe.set_devices((options.train_device,))
 
-
 solverstates = pygt.getSolverStates(solver_config.snapshot_prefix);
 
 # First training method
@@ -134,9 +170,5 @@ if (len(solverstates) == 0 or solverstates[-1][0] < solver_config.max_iter):
         solver.restore(solverstates[-1][1])
     pygt.train(solver, test_net, datasets, [], options)
     
-
-
-
-
 
 
