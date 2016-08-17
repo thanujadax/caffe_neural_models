1c81c871e259c0e83863fcf5935e966cb744698c
Thanuja Ambegoda <thanujadax@gmail.com>
2016-08-17 12:57:42 +0200

    modified for 0,1 lables

---
+++ pygt_uvisual_cremi/mknet.py
@@ -0,0 +1,70 @@
+from __future__ import print_function
+import h5py
+import numpy as np
+from numpy import float32, int32, uint8, dtype
+import sys
+
+# Relative path to where PyGreentea resides
+pygt_path = '../../PyGreentea'
+
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), pygt_path))
+
+# Other python modules
+import math
+
+# Load PyGreentea
+import PyGreentea as pygt
+
+
+netconf = pygt.netgen.NetConf()
+
+netconf.use_batchnorm = False
+netconf.dropout = 0.2
+
+netconf.fmap_start = 64
+
+#sk1 = pygt.netgen.SKNetConf()
+#sk1.sknet_conv = [[8],[6],[4]]
+
+#sk2 = pygt.netgen.SKNetConf()
+#sk2.sknet_padding = [44,44]
+#sk2.sknet_conv = [[6],[4],[2]]
+
+netconf.u_netconfs[0].unet_depth = 4
+netconf.u_netconfs[0].unet_fmap_inc_rule = lambda fmaps: int(math.ceil(float(fmaps) * 2))
+netconf.u_netconfs[0].unet_fmap_dec_rule = lambda fmaps: int(math.ceil(float(fmaps) / 2))
+netconf.u_netconfs[0].unet_downsampling_strategy = [[2],[2],[2],[2]]
+# netconf.u_netconfs[0].sk_netconfs = [None,sk1,sk2]
+# netconf.u_netconfs = [netconf.u_netconfs[0],netconf.u_netconfs[0]]
+
+
+netconf.loss_function = "softmax"
+inshape,outshape,fmaps = pygt.netgen.compute_valid_io_shapes(netconf,pygt.netgen.caffe_pb2.TRAIN,[400,400],[500,500],constraints=[None,lambda x: x[0], lambda x: x[1]])
+
+
+# We choose the maximum that still gives us 20 fmaps:
+index = [n for n, i in enumerate(fmaps) if i>=64][-1]
+print("Index to use: %s" % index)
+# Some patching to allow our new parameters
+netconf.input_shape = inshape[index]
+netconf.output_shape = outshape[index]
+netconf.fmap_start = 64
+netconf.fmap_output = 9
+
+netconf.loss_function = "softmax"
+train_net_softmax, test_net_softmax, tikz_train, tikz_test = pygt.netgen.create_nets(netconf)
+#netconf.loss_function = "malis"
+#train_net_conf_malis, test_net_conf, train_net_tikzgraph, test_net_tikzgraph = pygt.netgen.create_nets(netconf)
+
+with open('net_train_softmax.prototxt', 'w') as f:
+    print(train_net_softmax, file=f)
+with open('net_test_softmax.prototxt', 'w') as f:
+    print(test_net_softmax, file=f)
+    
+#with open('trainnet.tex', 'w') as f:
+#    print(train_net_tikzgraph, file=f)
+#with open('testnet.tex', 'w') as f:
+#    print(test_net_tikzgraph, file=f)
+
---
+++ pygt_uvisual_cremi/process.py
@@ -0,0 +1,108 @@
+from __future__ import print_function
+import h5py
+import numpy as np
+from numpy import float32, int32, uint8, dtype
+import sys
+
+from cremi import Annotations, Volume
+from cremi.io import CremiFile
+import random
+
+# Relative path to where PyGreentea resides
+pygt_path = '../../PyGreentea'
+
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), pygt_path))
+
+# Other python modules
+import math
+
+# Load PyGreentea
+import PyGreentea as pygt
+import glob
+from PIL import Image
+
+
+test_net_file = 'net_test_softmax.prototxt'
+test_device = 0
+
+pygt.caffe.set_devices((test_device,))
+
+caffemodels = pygt.getCaffeModels('net');
+
+test_net = pygt.init_testnet(test_net_file, trained_model=caffemodels[-1][1], test_device=test_device)
+'''
+Input option 1
+Load the datasets - hdf5
+'''
+'''
+hdf5_raw_file = '../dataset_06/fibsem_medulla_7col/tstvol-520-1-h5/img_normalized.h5'
+hdf5_gt_file = '../dataset_06/fibsem_medulla_7col/tstvol-520-1-h5/groundtruth_seg.h5'
+# hdf5_aff_file - affinity ground truth. if you train with euclid, ignore this one
+# hdf5_aff_file = '../dataset_06/fibsem_medulla_7col/tstvol-520-1-h5/groundtruth_aff.h5'
+
+# read hdf5 files
+hdf5_raw = h5py.File(hdf5_raw_file, 'r')
+hdf5_gt = h5py.File(hdf5_gt_file, 'r')
+# ignore for eucledian distance
+# hdf5_aff = h5py.File(hdf5_aff_file, 'r')
+
+# from this point, the data is in numpy arrays
+hdf5_raw_ds = pygt.normalize(np.asarray(hdf5_raw[hdf5_raw.keys()[0]]).astype(float32), -1, 1)
+hdf5_gt_ds = np.asarray(hdf5_gt[hdf5_gt.keys()[0]]).astype(float32)
+# ignore for eucledian distance
+# hdf5_aff_ds = np.asarray(hdf5_aff[hdf5_aff.keys()[0]]).astype(float32)
+'''
+
+###############################################################
+'''
+Input option 2
+Load the datasets - individual tiff files in a directory
+'''
+# Load the datasets
+'''
+rawInputDir = '/home/thanuja/projects/data/dataset_01/train/raw';
+labelInputDir = '/home/thanuja/projects/data/dataset_01/train/labels'
+
+rawImagePath = sorted(glob.glob(rawInputDir+'/*.tif'))
+print("rawImagePath: {0}".format(rawImagePath))
+labelImagePath = sorted(glob.glob(labelInputDir+'/*.png'))
+numFiles = len(rawImagePath)
+
+raw_ds = [np.expand_dims(pygt.normalize(np.array(Image.open(rawImagePath[i]).convert('L'), 'f')),0) for i in range(0,1)]
+gt_ds = [np.array(Image.open(labelImagePath[i]).convert('L'), 'f') for i in range(0,1)]
+gt_ds_scaled = [np.expand_dims(np.floor(label/31),0) for label in gt_ds]
+print(gt_ds_scaled[0].shape)
+print(raw_ds[0].shape)
+'''
+
+'''
+Input format: cremi challenge
+'''
+hdf5_fileName = '/home/thanuja/DATA/cremi/train/hdf/sample_A_20160501.hdf'
+file = CremiFile(hdf5_fileName, "r")
+
+raw_ds = pygt.normalize(np.array(file.read_raw().data))
+# print(raw_ds[0].shape)
+
+datasets = []
+# for i in range(0,len(raw_ds)):
+for i in range(0,3):
+    dataset = {}
+    dataset['data'] = np.expand_dims(raw_ds[i],0)
+    # dataset['label'] = gt_ds_scaled[i]
+    datasets += [dataset]
+
+print(len(dataset)) # expected 125
+print(datasets[0]['data'].shape) # expected (1,1250,1250)
+
+
+pred_array = pygt.process(test_net, datasets)
+
+#pygt.dump_tikzgraph_maps(test_net, 'dump')
+
+outhdf5 = h5py.File('test_out.h5', 'w')
+outdset = outhdf5.create_dataset('main', np.shape(pred_array), np.float32, data=pred_array)
+outhdf5.close()
+
